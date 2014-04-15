bd08e8bb051bbeaab2c6593fe5d58efc8aa04bdb
RickyAgaric <rickyagaric@gmail.com>
2014-04-15 11:14:54 +0800

    update scripts and env file

--- bin/cc-clean.sh
+++ bin/cc-clean.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "Cleaning .keep files..."
-find ./ -name *.*.keep -exec rm {} +
+find ./ -name *.*.keep -exec rm -v {} +
 
 echo "Cleaning .contrib files..."
-find ./ -name *.*.contrib -exec rm {} +
+find ./ -name *.*.contrib -exec rm -v {} +
+
+echo "Cleaning .orig files..."
+find ./ -name *.*.orig -exec rm -v {} +
 
--- bin/net-through-firewall.sh
+++ bin/net-through-firewall.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+servers='9.115.21.163
+9.30.215.90
+9.5.5.42
+9.30.199.184'
 
-auto_telnet() {
-	expect -c "spawn telnet $1
-		expect name:
-		send $2\r
-		expect assword:
-		send $3\r
+
+throughFirewall() {
+	expect -c "
+		spawn telnet $1
+		expect {
+			default {exit}
+			\"name:\" {send $2\r;exp_continue}
+			\"assword:\" {send $3\r;exp_continue}
+		}
 		interact"
-} 
+}
 
 if [ "$1" == "" ]; then
-	auto_telnet "9.115.21.22" $INTRANET_ID $INTRANET_PWD
-	auto_telnet "9.30.215.90" $INTRANET_ID $INTRANET_PWD
-	auto_telnet "9.5.5.42" $INTRANET_ID $INTRANET_PWD
+	for server in ${servers}
+	do
+		throughFirewall ${server} ${INTRANET_ID} ${INTRANET_PWD}
+	done
 else
-	auto_telnet $1 $INTRANET_ID $INTRANET_PWD
+	throughFirewall ${1} ${INTRANET_ID} ${INTRANET_PWD}
 fi 
